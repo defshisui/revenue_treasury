8fe28ded11babba98bb26707b37de4debcd50de4
*Ayato15523* <*jomelbaisac16@gmail.com*>
2026-08-27 09:18:25 +0800

    update

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,10 +1,11 @@
 // src/routes/users.routes.ts
 import { Router } from 'express';
-import { getUsers, createUser } from '../controllers/users.controller.js';
+import { getUsers, createUser, deleteUser } from '../controllers/users.controller.js';
 
 const router = Router();
 
 router.get('/users', getUsers);
 router.post('/users', createUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
